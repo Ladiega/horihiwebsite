c029f5d627e9fb5ebbf9e8037364663e527f7bcb
Ladiega <Fokiuatodo@gmail.com>
2022-03-06 19:41:49 -0500

    ussing _app.js and layout

---
+++ pages/post.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+
+export default function Post (){
+    return(
+
+
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.heading}>
+                <p>Esta es la prueba de los la implementacion del Layout</p>
+                <Link href="/">
+                <a>Home</a>
+                </Link>
+            </section>
+        </Layout>
+    )
+}
