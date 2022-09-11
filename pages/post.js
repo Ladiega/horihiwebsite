a532332375fa523fcfb29a148ec98bbc39a6c643
Tu Nombre <fokiuatodo@gmail.com>
2022-09-11 16:16:50 -0500

    ham menu it working in /post

--- pages/post.js
+++ pages/post.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-
+import Ham from '../components/hamButton/index'
 import React,{Component} from 'react'
 
 export default function Post (){
@@ -15,6 +15,9 @@ export default function Post (){
                 <title>{siteTitle}</title>
                
             </Head>
+            <section>
+                <Ham></Ham>
+            </section>
 
             <section className={utilStyles.heading}>
                 <p>Esta es la prueba de los la implementacion del Layout</p>
