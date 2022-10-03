4859dc25163510fce771e14f84dc89622df88f9b
Tu Nombre <fokiuatodo@gmail.com>
2022-10-03 10:18:03 -0500

    styling img borders example in about.js

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import Navbar from '../components/navbar/index'
-
+import styles from '../components/about.module.css'
 import Link from 'next/link'
 import Images from 'next/image'
 import tebori from '../public/img/about/tebori.jpg'
@@ -11,11 +11,32 @@ import ReactPlayer from 'react-player'
 export default function AboutMe(){
     return(
         <div className="about_global_container">
-            <Navbar/>
+            
+            <div className="about_container4">Container4
+                <div className='player_container'>
+                <ReactPlayer 
+                className='videotebori'
+                url="video/tebori.mp4"
+                
+                width={300}
+                height={250}
+
+                loop={true}
+                playing={true}
+                muted={true}
+                />
+                </div>
+
+
+            </div>
+            
+                       
+            <Navbar className='nav_container'/>
+            
             <div className="about_container1">
-               <div className="about_img">
-                <Images
-               
+               <div className="about_img_container">
+                    <Images
+                className={styles.img}
                 alt="tebori"
                 src={tebori}
                 height={100}
@@ -30,16 +51,7 @@ export default function AboutMe(){
             <div className="about_container3">
                 Container3
             </div>
-            <div className="about_container4">Container4
-                <ReactPlayer 
-                className='videotebori'
-                url="video/tebori.mp4"
-                
-                loop={true}
-                playing={true}
-                muted={true}
-                />
-            </div>
+            
            
             
         <style jsx>{`
@@ -58,13 +70,30 @@ export default function AboutMe(){
                 text-align:center;
                                
             }
+           
             .about_container4{
                 display:flex;
+                position:absolute;
                 background-color:black;
                 justify-content:center;
-             
+                color:white;
+                opacity:70%;
+                top:3rem;
+                left:35%;   
             }
-            
+            .player_container {
+                background-color:red;
+            }
+            .about_img_container{
+                background-color:salmon;
+                position:relative;
+                border-radius:4rem;
+                
+            }
+            .about_img_container .about_img{
+                border-radius:3rem;
+            }
+           
          
             `}
 
