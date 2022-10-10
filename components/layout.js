417a9e3e6d8d55f48d6df9605848058f14ae00c5
La Diega <fokiuatodo@hotmail.com>
2022-10-10 14:35:44 -0500

    update paintings photos

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="hori-hi website"
         />
         <meta
           property="og:image"
